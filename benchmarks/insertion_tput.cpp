d1f3535404ec2c200dcf2628b8c5c1f92b39e797
Douglas Rumbaugh <dbr4@psu.edu>
2023-11-16 11:59:52 -0500

    insert_tput: minor adjustments

--- benchmarks/insertion_tput.cpp
+++ benchmarks/insertion_tput.cpp
@@ -21,29 +21,24 @@ typedef de::DynamicExtension<Rec, ISAM, Q> Ext;
 
 int main(int argc, char **argv) {
 
-    auto extension = Ext(1000, 2, 1);
+    auto extension = new Ext(10000, 2, 1);
 
-    size_t n = 1000000;
-    size_t per_trial = 100;
-
-    std::vector<int64_t> latencies;
+    size_t n = 1000000000;
+    size_t per_trial = 1000;
 
     TIMER_INIT();
     for (int64_t i=0; i<n; i+=per_trial) {
         TIMER_START();
         for (int64_t j=0; j<per_trial; j++) {
             Rec r = {i+j, i+j};
-            extension.insert(r);
+            extension->insert(r);
         }
         TIMER_STOP();
+        auto insert_lat = TIMER_RESULT();
 
-        auto res = TIMER_RESULT();
-
-        latencies.push_back(TIMER_RESULT());
+        fprintf(stdout, "%ld\t%ld\t%ld\n", extension->get_record_count(), insert_lat, per_trial);
     }
 
-    for (size_t i=0; i<latencies.size(); i++) {
-        fprintf(stdout, "%ld\t%ld\t%ld\n", (1+i)*per_trial, latencies[i], per_trial);
-    }
+    fflush(stderr);
 }
 
